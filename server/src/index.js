f2f6a7ff0cd62f93d2266c7ce593adf90c426a39
carlosmz96 <carlosmz96esp@gmail.com>
2021-02-27 10:02:47 +0100

    Configuración de cabeceras HTTP

--- server/src/index.js
+++ server/src/index.js
@@ -13,6 +13,16 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+// Configuracion de cabeceras http
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+
 // Rutas base
 app.use('/api', userRoutes);
 
