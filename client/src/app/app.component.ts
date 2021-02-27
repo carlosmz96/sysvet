a5d38313182e2785414886867e7ec57ca6d55ad9
carlosmz96 <carlosmz96esp@gmail.com>
2021-02-27 13:10:34 +0100

    Interfaz de inicio comenzada
    Implementación de la interfaz de inicio comenzada, modificación del modelo de usuario y scripts de popper y bootstrap añadidos al index.html

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from './models/Usuario';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SYSVET';
+  public title = 'SYSVET';
+  public usuario: Usuario;
+  public identity = false;
+  public token = null;
+
+  constructor() {
+    this.usuario = new Usuario('','','','','','','','','');
+  }
+
 }
--- client/src/app/models/Usuario.ts
+++ client/src/app/models/Usuario.ts
@@ -1,13 +1,97 @@
 export class Usuario {
-    constructor(
-        private dni: string,
-        private nombre: string,
-        private apellidos: string,
-        private email: string,
-        private pass: string,
-        private rol: string,
-        private telefono: string,
-        private direccion: string,
-        private foto: string
-    ) {}
+
+    private dni: string;
+    private nombre: string;
+    private apellidos: string;
+    private email: string;
+    private pass: string;
+    private rol: string;
+    private telefono: string;
+    private direccion: string;
+    private foto: string;
+
+    constructor(dni: string, nombre: string, apellidos: string, email: string, pass: string, rol: string, telefono: string, direccion: string, foto: string) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.pass = pass;
+        this.rol = rol;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.foto = foto;
+    }
+
+    public getDni(): string {
+        return this.dni;
+    }
+
+    public setDni(dni: string): void {
+        this.dni = dni;
+    }
+
+    public getNombre(): string {
+        return this.nombre;
+    }
+
+    public setNombre(nombre: string): void {
+        this.nombre = nombre;
+    }
+
+    public getApellidos(): string {
+        return this.apellidos;
+    }
+
+    public setApellidos(apellidos: string): void {
+        this.apellidos = apellidos;
+    }
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public setEmail(email: string): void {
+        this.email = email;
+    }
+
+    public getPass(): string {
+        return this.pass;
+    }
+
+    public setPass(pass: string): void {
+        this.pass = pass;
+    }
+
+    public getRol(): string {
+        return this.rol;
+    }
+
+    public setRol(rol: string): void {
+        this.rol = rol;
+    }
+
+    public getTelefono(): string {
+        return this.telefono;
+    }
+
+    public setTelefono(telefono: string): void {
+        this.telefono = telefono;
+    }
+
+    public getDireccion(): string {
+        return this.direccion;
+    }
+
+    public setDireccion(direccion: string): void {
+        this.direccion = direccion;
+    }
+
+    public getFoto(): string {
+        return this.foto;
+    }
+
+    public setFoto(foto: string): void {
+        this.foto = foto;
+    }
+
 }
