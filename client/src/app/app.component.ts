afa0c1912c6321e053439a61f7ae9d44534e3008
carlosmz96 <carlosmz96esp@gmail.com>
2021-03-01 13:55:40 +0100

    Inicio de sesión implementado
    Se ha implementado la vista del inicio de sesión, además de crear un servicio que accede al api y así poder iniciar sesión desde el cliente.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Usuario } from './models/Usuario';
+import { UsuarioService } from './usuario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsuarioService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'SYSVET';
   public usuario: Usuario;
-  public identity = false;
-  public token = null;
+  public identity: any = false;
+  public token: any = null;
+  public mensajeError: string = "";
 
-  constructor() {
-    this.usuario = new Usuario('','','','','','','','','');
+  constructor(
+    private usuarioService: UsuarioService
+  ) {
+    this.usuario = new Usuario('', '', '', '', '', '', '', '', '');
+  }
+
+  /**
+   * Método que se ejecuta al iniciar el componente
+   */
+  ngOnInit(): void {
+    this.identity = this.usuarioService.getIdentity();
+    this.token = this.usuarioService.getToken();
+
+    console.log(this.identity);
+    console.log(this.token);
+  }
+
+  /**
+   * Método encargado de iniciar sesión en la aplicación
+   */
+  public onSubmit(): void {
+    // Conseguir los datos del usuario identificado
+    this.usuarioService.iniciarSesion(this.usuario).subscribe(
+      response => {
+        this.identity = response.user;
+        if (!this.identity.dni) {
+          this.mensajeError = "El usuario no está correctamente identificado";
+        } else {
+          // Crear elemento en localStorage para tener al usuario logueado
+          localStorage.setItem('identity', JSON.stringify(this.identity));
+
+          // Conseguir el token para enviarselo a cada petición http
+          this.usuarioService.iniciarSesion(this.usuario, true).subscribe(
+            response => {
+              this.token = response.token;
+              if (this.token.length <= 0) {
+                this.mensajeError = "El token no se ha generado";
+              } else {
+                // Crear elemento en localStorage para tener el token disponible
+                localStorage.setItem('token', this.token);
+              }
+            },
+            error => {
+              this.mensajeError = error.error.message;
+            }
+          );
+        }
+      },
+      error => {
+        this.mensajeError = error.error.message;
+      }
+    );
   }
 
 }
---
+++ client/src/app/usuario.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { Usuario } from './models/Usuario';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  public url: string;
+  public identity: any;
+  public token: any;
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    this.url = "http://localhost:3000/api/";
+  }
+
+  /**
+   * Método encargado de llamar al api para iniciar sesión
+   * @param user_to_login Usuario a loguearse
+   * @param gethash Token generado
+   */
+  public iniciarSesion(user_to_login: any, gethash: any = null):Observable<any> {
+    if(gethash != null) {
+      user_to_login.gethash = gethash;
+    }
+
+    const json = JSON.stringify(user_to_login);
+    const params = json;
+
+    const headers = new HttpHeaders().set('Content-Type','application/json');
+
+    return this.httpClient.post(this.url + 'login', params, {headers: headers}).pipe(map(res => res));
+  }
+
+  public getIdentity() {
+    const identity = JSON.parse(localStorage.getItem('identity')!);
+
+    if(identity != "undefined"){
+      this.identity = identity;
+    }else{
+      this.identity = null;
+    }
+
+    return this.identity;
+  }
+
+  public getToken() {
+    const token = JSON.parse(localStorage.getItem('token')!);
+
+    if(token != "undefined"){
+      this.token = token;
+    }else{
+      this.token = null;
+    }
+
+    return this.token;
+  }
+
+}
